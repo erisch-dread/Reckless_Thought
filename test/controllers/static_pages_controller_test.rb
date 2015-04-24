906e1aa6ded113d9fff7b3ac202833b0774553bd
Erisch Dread <erisch.dread@gmail.com>
2015-04-24 19:54:23 -0400

    Added more dynamic test cases.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,16 +1,19 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "Reckless Thought"
+  end
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Reckless Thought"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact | Reckless Thought"
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
