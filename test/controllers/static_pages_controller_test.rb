062e80dd1466546c84a41a9ac5faafd64bcb75c1
Erisch Dread <erisch.dread@gmail.com>
2015-04-24 19:41:34 -0400

    Finished with static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Reckless Thought"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title", "Contact | Reckless Thought"
   end
 
 end
