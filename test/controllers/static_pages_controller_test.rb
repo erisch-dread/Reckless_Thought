62099bf93374864801102d96880aafaa744ce261
Erisch Dread <erisch.dread@gmail.com>
2015-05-11 09:44:17 -0400

    Most of the base layout has been set up (though the content isn't complete)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -16,4 +16,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "Contact | #{@base_title}"
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
+
 end
